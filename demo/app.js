17ce4dfb76898cf5fa6949ea685ad454db049960
CremAlex <thomb45@gmail.com>
2016-08-09 14:18:28 +0200

    update readme

--- demo/app.js
+++ demo/app.js
@@ -18,7 +18,5 @@ ReactDOM.render( <Board elements={ umlElements } firebaseUrl="flickering-torch-7
 */
 
 ReactDOM.render( <Board firebaseUrl="flickering-torch-7474.firebaseio.com" boardKey="-KFPEXh-RGkG5uXtgX4M" gmapsApiKey="AIzaSyBN5nAMeOkCj_fzZs9gw4BPEfsRZAMH5cQ" />, document.getElementById( 'app-container' ) );
-//ReactDOM.render( <Timeline widgetId={'760478435647057920'} onLoad={() => console.log('Timeline is loaded!')}/>, document.getElementById( 'app-container' ) );
 
 
-//<a className="twitter-timeline" href="https://twitter.com/thombsoncrema">Tweets Liked by @TwitterDev</a>
